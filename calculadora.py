a0351fbacf5ad8ac7017bd1b3c4301d2efc28c83
macalderonv <macv@delta14.aulas.gsyc.urjc.es>
2018-01-30 12:38:23 +0100

    v2 calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -3,5 +3,26 @@
 
 import sys
 
-print(sys.argv)
-print(sys.argv[1])
+#print(sys.argv)
+#print(sys.argv[1])
+    
+operador = sys.argv[2] 
+if operador == '+':
+    suma = int(sys.argv[1]) + int(sys.argv[3])
+    print("El resultado de la suma " + str(sys.argv[1]) + " + " + str(sys.argv[3]) + " = " + str(suma))
+elif operador == '-':
+    resta = int(sys.argv[1]) - int(sys.argv[3])
+    print("El resultado de la resta " + str(sys.argv[1]) + " - " + str(sys.argv[3]) + " = " + str(resta)) 
+elif operador == '*':
+   multiplicacion = int(sys.argv[1]) * int(sys.argv[3])
+   print("El resultado de la multiplicacion " + str(sys.argv[1]) + " * " + str(sys.argv[3]) + " = " + str(multiplicacion)) 
+elif operador == '/':
+    try:
+        division = int(sys.argv[1]) / int(sys.argv[3])
+        print("El resultado de la division " + str(sys.argv[1]) + " / " + str(sys.argv[3]) + " = " + str(division)) 
+    except ZeroDivisionError:
+        print("No se puede dividir entre 0")
+else:
+    print("operador erroneo")
+            
+
