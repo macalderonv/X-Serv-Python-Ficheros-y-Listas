d12e8a63270d3b9e7025aae59e590e34187a37df
macalderonv <macv@gamma32.aulas.gsyc.urjc.es>
2018-02-01 09:27:40 +0100

    v3 calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -6,23 +6,23 @@ import sys
 #print(sys.argv)
 #print(sys.argv[1])
     
-operador = sys.argv[2] 
+operador = sys.argv[1] 
 if operador == '+':
-    suma = int(sys.argv[1]) + int(sys.argv[3])
-    print("El resultado de la suma " + str(sys.argv[1]) + " + " + str(sys.argv[3]) + " = " + str(suma))
+    suma = int(sys.argv[2]) + int(sys.argv[3])
+    print("El resultado de la suma " + str(sys.argv[2]) + " + " + str(sys.argv[3]) + " = " + str(suma))
 elif operador == '-':
-    resta = int(sys.argv[1]) - int(sys.argv[3])
-    print("El resultado de la resta " + str(sys.argv[1]) + " - " + str(sys.argv[3]) + " = " + str(resta)) 
+    resta = int(sys.argv[2]) - int(sys.argv[3])
+    print("El resultado de la resta " + str(sys.argv[2]) + " - " + str(sys.argv[3]) + " = " + str(resta)) 
 elif operador == '*':
-   multiplicacion = int(sys.argv[1]) * int(sys.argv[3])
-   print("El resultado de la multiplicacion " + str(sys.argv[1]) + " * " + str(sys.argv[3]) + " = " + str(multiplicacion)) 
+   multiplicacion = int(sys.argv[2]) * int(sys.argv[3])
+   print("El resultado de la multiplicacion " + str(sys.argv[2]) + " * " + str(sys.argv[3]) + " = " + str(multiplicacion)) 
 elif operador == '/':
     try:
-        division = int(sys.argv[1]) / int(sys.argv[3])
-        print("El resultado de la division " + str(sys.argv[1]) + " / " + str(sys.argv[3]) + " = " + str(division)) 
+        division = int(sys.argv[2]) / int(sys.argv[3])
+        print("El resultado de la division " + str(sys.argv[2]) + " / " + str(sys.argv[3]) + " = " + str(division)) 
     except ZeroDivisionError:
         print("No se puede dividir entre 0")
 else:
-    print("operador erroneo")
+    print("operador erroneo. si es una multiplicacion hay que poner \*")
             
 
